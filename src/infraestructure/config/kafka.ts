8d632d5628ac6908b2340b8b17c5d940673d06a4
daniel palacio <palaciodaniel275@gmail.com>
2025-04-14 14:41:52 -0500

    se creo el microservicio de orden de compra, se conecto con mongo db, kafka y kafdrop para recibir y guardar los evetos de compra, tales como el user id y el el item con sus caracteristicas

--- src/infraestructure/config/kafka.ts
+++ src/infraestructure/config/kafka.ts
@@ -2,5 +2,5 @@ import { Kafka } from 'kafkajs';
 
 export const kafka = new Kafka({
   clientId: 'order-service',
-  brokers: ['localhost:9092'] // cambia a 'kafka:9092' si estás usando Docker Compose
+  brokers: ['localhost:29092']
 });
